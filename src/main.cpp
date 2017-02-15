3feb9e3b6510c57674e9a913b5eaf2b18ffb9bfe
Dehua Cheng <dehua.cheng@usc.edu>
2017-02-15 15:00:15 -0800

    add cmd input for testing

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
 
-    Graph g(argv[1], stoi(argv[2]));
+    Graph g(argv[1]);
     cout << "Graph loaded" << endl;
     // g.addEdge(0, 1, 8);
     // g.addEdge(1, 2, 3);
@@ -26,18 +26,24 @@ int main(int argc, char **argv)
 
     g.verbose = 1;
     Timer tm;
-    g.getKCC();
+    g.forceC = stoi(argv[3]);
+    g.batchM = stoi(argv[4]);
+    g.getKCC(stoi(argv[2]));
     if (g.verbose)
         cout << "Time for KCC: " << tm.CheckTimer() << endl;
     cout << g.kCC.size() << endl;
-    for (auto &v : g.kCC)
-    {
-        cout << v.size() << endl;
-        // for (auto &i : v)
-        // {
-        //     cout << i << " ";
-        // }
-        // cout << endl;
-    }
+    // for (auto &v : g.kCC)
+    // {
+    //     cout << v.size() << endl;
+    //     // for (auto &i : v)
+    //     // {
+    //     //     cout << i << " ";
+    //     // }
+    //     // cout << endl;
+    // }
+    cout << "Time for merge:\t" << g.timeForMerge << endl;
+    cout << "Time for kMas:\t" << g.timeForKMas << endl;
+    cout << "Number of Mas Visit:\t" << g.cntVisit << endl;
+    // cout << "Number of update kMas Saved:\t" << g.cntKMasSave << endl;
     return 0;
 }
